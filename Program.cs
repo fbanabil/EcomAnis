094b143594d874966a702130df2b5e8bc9cbb208
fbanabil <nabil.ea.ea@gmail.com>
2024-11-23 16:49:59 +0600

    Hello

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
-
-
 app.UseHttpsRedirection();
+
+app.MapGet("/hello",()=>
+{
+    return "Hello from get method";
+});
 
 app.Run();
